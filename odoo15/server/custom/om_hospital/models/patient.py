0b982ea8ced981d2faa67c5ed3abb90ec361d85f
HanZawNyine <hanzawnyine143@gmail.com>
2022-03-24 10:51:16 +0630

    create model

---
+++ odoo15/server/custom/om_hospital/models/patient.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _, tools, api
+
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
+    _description = "Hospital Patient"
+
+    name = fields.Char(string='Name', required=True, translate=True, tracking=True)
+    age = fields.Integer(string='Age', tracking=True)
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    ], required=True, default='other', tracking=True)
+    note = fields.Text(string='Description', tracking=True)
+    
