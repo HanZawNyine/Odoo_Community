48f9f964a85c5acc202c0b75a96fd5117ede6ecb
HanZawNyine <hanzawnyine143@gmail.com>
2022-03-26 18:29:05 +0630

    Create record from code

--- odoo15/server/custom/om_hospital/wizard/create_appointment.py
+++ odoo15/server/custom/om_hospital/wizard/create_appointment.py
@@ -6,8 +6,12 @@ class CreateAppointmentWizard(models.TransientModel):
     _name = "create.appointment.wizard"
     _description = "Create Appointment Wizard"
 
-    name = fields.Char(string='Name', required=True, tracking=True)
-    patient_id = fields.Many2one('hospital.patient',string="Patient",required=True)
+    date_appointment = fields.Date(string='Date', required=True, tracking=True)
+    patient_id = fields.Many2one('hospital.patient', string="Patient", required=True)
 
     def action_create_appointment(self):
-        pass
+        vals = {
+            'patient_id': self.patient_id.id,
+            'date_appointment': self.date_appointment
+        }
+        self.env['hospital.appointment'].create(vals)
